7c110b7914c9ee65b7127d8998b830cc35491468
ntphat1711999 <tanphat1711999@gmail.com>
2021-01-10 14:29:57 +0700

    init project

--- reviewschooladmin/src/services/index.js
+++
@@ -1,3 +0,0 @@
-export { default as boardApi } from "./boardApi";
-export { default as authApi } from "./authApi";
-export { default as cardApi } from "./cardApi";
--- reviewschooladmin/src/store/reducers.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { reducers } from "./../redux";
-
-export default function createReducer() {
-  return combineReducers(reducers);
-}
